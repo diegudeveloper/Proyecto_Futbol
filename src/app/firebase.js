d1ba0d9cbf8da8ad99bf2036ca8c01c6fefc1a0e
diegudeveloper <diegudeveloper@gmail.com>
2023-03-21 21:01:15 +0100

    Inicio del proyecto Futbol

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_8pDsbWKE_3gXBzjM_VEdo_rHSuFl92E",
+  authDomain: "proyecto-futbol-79e48.firebaseapp.com",
+  projectId: "proyecto-futbol-79e48",
+  storageBucket: "proyecto-futbol-79e48.appspot.com",
+  messagingSenderId: "929917736621",
+  appId: "1:929917736621:web:1f36a1d6b7fcf337581824"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import './app/firebase.js'
+console.log('hello world')

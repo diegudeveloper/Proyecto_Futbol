1000d39c69379db6efa766da2340354669a64993
diegudeveloper <diegudeveloper@gmail.com>
2023-03-23 20:43:46 +0100

    configuracion del login y logout

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+import { auth } from "./app/firebase.js"; 
+import { loginCheck } from "./app/loginCheck.js"; 
+
 import './app/signupForm.js';
+import './app/logout.js';
+
+
+onAuthStateChanged(auth, async (user) => {
+    loginCheck(user)
+    // if (user) {
+    //     loginCheck(user)
+    // } else {
+    //     loginCheck(user)
+    // }
+})
